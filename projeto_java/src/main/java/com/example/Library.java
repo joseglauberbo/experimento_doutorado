91710822af2224cd5faa01fe390e20c62c0361ed
joseglauberbo <jose.oliveira@ccc.ufcg.edu.br>
2024-05-21 10:39:07 -0300

    Refactoring with Inline Method without remove Feature Envy;

--- projeto_java/src/main/java/com/example/Library.java
+++ projeto_java/src/main/java/com/example/Library.java
@@ -8,16 +8,17 @@ public class Library {
     public Library(List<Book> books) {
         this.books = books;
     }
-    
-    public String getBookDetails(Book book) {
-        return book.getTitle() + " by " + book.getAuthor() + ", ISBN: " + book.getIsbn();
-    }
 
     // Exibe muitos detalhes da classe Book.
     public void generateLibraryReport() {
         StringBuilder report = new StringBuilder();
         for (Book book : books) {
-            report.append(getBookDetails(book)).append("\n");
+            report.append(book.getTitle())
+                  .append(" by ")
+                  .append(book.getAuthor())
+                  .append(", ISBN: ")
+                  .append(book.getIsbn())
+                  .append("\n");
         }
         System.out.println(report.toString());
     }
